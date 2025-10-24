ee54f3c5fea25fc7854dba9fb5568fc9af2d5d35
Shiftnace <shiftnace@gmail.com>
2025-10-24 11:33:31 +0545

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
-// Add smooth scrolling effect
+// Smooth scrolling for internal links
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener("click", function(e) {
+  anchor.addEventListener("click", function (e) {
     e.preventDefault();
-    document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior: "smooth"
-    });
+
+    const target = document.querySelector(this.getAttribute("href"));
+    if (target) {
+      target.scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+      });
+    }
   });
 });
